be54aeb662a8bda098e2be24988ab765b1db44cc
Vequinox <46204139+Vequinox@users.noreply.github.com>
2019-09-10 19:27:35 -0500

    Added basic scripts for start of slab creation automation

---
+++ scripts/GenerateSlabBlockstates.ps1
@@ -0,0 +1,10 @@
+﻿$SlabData = Get-Content -Path SlabAPINames.txt
+$BlockstateBlueprint = Get-Content -Path BlockstateBlueprint.txt
+
+foreach($SlabInfo in $SlabData){
+    $ApiName,$DisplayName,$BottomTexture,$TopTexture,$SideTexture,$DoubleTexture,$Material,$MaterialColor,$Hardness,$Resistance = $SlabInfo -split '\t'
+    
+    $Blockstate = $BlockstateBlueprint.Replace('{BOTTOM}', $ApiName.Replace('_', ''))
+    $ApiNameNoUnd = $ApiName.Replace('_', '')
+    $Blockstate | Out-File "Output\blockstates\$ApiNameNoUnd.json" -Encoding utf8
+}
---
+++ scripts/GenerateSlabModelsCustom.ps1
@@ -0,0 +1,10 @@
+﻿$SlabData = Get-Content -Path SlabAPINames.txt
+$ModelDataBlueprint = Get-Content -Path BlockModelBlueprintCustom.txt
+
+foreach($SlabInfo in $SlabData){
+    $ApiName,$DisplayName,$BottomTexture,$TopTexture,$SideTexture,$DoubleTexture,$Material,$MaterialColor,$Hardness,$Resistance = $SlabInfo -split '\t'
+    
+    $ModelData = $ModelDataBlueprint.Replace('{BOTTOM}', $BottomTexture).Replace('{TOP}', $TopTexture).Replace('{SIDE}', $SideTexture)
+    $ApiNameNoUnd = $ApiName.Replace('_', '')
+    $ModelData | Out-File "Output\models\blocks\$ApiNameNoUnd.json" -Encoding utf8
+}
