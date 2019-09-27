8f189cfa51a63e8456ee3bc1f3807f9a1f2daa53
Vequinox <46204139+Vequinox@users.noreply.github.com>
2019-09-26 21:10:46 -0500

    Added a script for generating loot tables

---
+++ scripts/GenerateLootTables.ps1
@@ -0,0 +1,10 @@
+﻿$BlockData = Get-Content -Path BlockAPINames.txt
+$LootTableBlueprint = Get-Content -Path LootTableBlueprint.txt
+
+foreach($BlockInfo in $BlockData){
+    $ApiName,$_,$_,$_,$_,$_,$_,$_,$_,$_,$_,$_,$_ = $BlockInfo -split '\t'
+    
+    $ModelData = $LootTableBlueprint.Replace('{API_NAME}', $ApiName.Replace('_', ''))
+    $ApiNameNoUnd = $ApiName.Replace('_', '')
+    $ModelData | Out-File "Output\loottables\$ApiNameNoUnd.json" -Encoding utf8
+}
