4ea7320ee253f42390a1df604de6ac708331a5a2
Vequinox <46204139+Vequinox@users.noreply.github.com>
2019-09-26 21:10:35 -0500

    Updated some scripts

--- scripts/GenerateLang.ps1
+++ scripts/GenerateLang.ps1
@@ -4,6 +4,7 @@ $LangBlueprint = Get-Content -Path LangBlueprint.txt
 foreach($BlockInfo in $BlockData){
     $ApiName,$DisplayName,$_,$_,$_,$_,$_,$_,$_,$Material,$MaterialColor,$Hardness,$Resistance = $BlockInfo -split '\t'
     
-    $LangEntry = $LangBlueprint.Replace('{API_NAME}', $ApiName).Replace('{DISPLAY_NAME}', $DisplayName)
+    $ApiNameNoUnd = $ApiName.Replace('_', '')
+    $LangEntry = $LangBlueprint.Replace('{API_NAME}', $ApiNameNoUnd).Replace('{DISPLAY_NAME}', $DisplayName)
     $LangEntry >> "Output\LangAddon.txt"
 }
