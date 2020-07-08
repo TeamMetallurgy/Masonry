e9d3cb4436ed66fc44b29f6c4d58c149c11f2c1c
Vequinox <josephrogerahrens@gmail.com>
2020-07-07 20:31:23 -0500

    Created a script to update 1.15 wall blockstates to 1.16 wall blockstates

---
+++ scripts/TranslateWallModel15to16.ps1
@@ -0,0 +1,8 @@
+﻿$files = Get-ChildItem "C:\Users\josep\Desktop\Masonry116\src\main\resources\assets\masonry\blockstates\" -Filter *wall.json
+$blockstate16 = Get-Content -Path blockstate16template.txt
+
+foreach($file in $files){
+    $filename = $file.FullName.Replace('wall.json', '').Replace('C:\Users\josep\Desktop\Masonry116\src\main\resources\assets\masonry\blockstates\', '')
+    $newBlockstate = $blockstate16.Replace('{name}', $filename)
+    $newBlockstate | Out-File $file.FullName -Encoding utf8
+}
