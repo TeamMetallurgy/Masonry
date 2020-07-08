2d1ff1130db73010dd002e20895745a3a47ea0d4
Vequinox <josephrogerahrens@gmail.com>
2020-07-07 21:15:16 -0500

    Created a script to add side tall wall models

---
+++ scripts/GenerateWallSideTallModelsFrom15SideModels.ps1
@@ -0,0 +1,8 @@
+﻿$files = Get-ChildItem "C:\Users\josep\Desktop\Masonry116\src\main\resources\assets\masonry\models\block" -Filter *side.json
+
+foreach($file in $files){
+    $contents = Get-Content $file.FullName
+    $contents = $contents.Replace('wall_side', 'wall_side_tall')
+    $path = $file.FullName.Replace('side.json', 'sidetall.json')
+    $contents | Out-File $path -Encoding utf8
+}
